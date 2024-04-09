3db9d3b5b61d9e866a5e1a04157aaab8cf024964
Alexandra Vegas <alexvegasdev@gmail.com>
2024-04-08 22:24:45 -0500

    models

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Record extends Model
 {
+    use SoftDeletes;
     use HasFactory;
     /**
      * The attributes that are mass assignable.
@@ -14,12 +16,12 @@ class Record extends Model
      * @var array<int, string>
      */
     protected $fillable = [
+        'user_id',
         'loan_date',
-        'return_date',
-        'user_id'
+        'return_date'
     ];
 
-    public function customer(){
+    public function user(){
         return $this->belongsTo(User::class);
     }
 
